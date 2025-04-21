3ebc75ebece3f67e395c4662adbbe12b69d36579
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-04-21 15:11:14 +0200

    REFACTOR: remove unused imports of TeamDao.java

--- src/main/java/org/torneo/business/TeamDao.java
+++ src/main/java/org/torneo/business/TeamDao.java
@@ -1,9 +1,7 @@
 package org.torneo.business;
 
-import org.torneo.model.Player;
 import org.torneo.model.Team;
 
-import java.util.List;
 
 public interface TeamDao extends GenericCrud<Team, String> {
 }
